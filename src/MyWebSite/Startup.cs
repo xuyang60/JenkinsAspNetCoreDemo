ff62544752d9def8a301e43b476c42d648047a4e
fxu <fxu@senparc.com>
2020-09-01 11:51:16 +0800

    获取真实IP

--- src/MyWebSite/Startup.cs
+++ src/MyWebSite/Startup.cs
@@ -29,6 +29,10 @@ namespace MyWebSite
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
